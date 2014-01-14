61aa4b857bd0cb885d7177262c25bd08ba80a26b
Jessica Chan <jessicachan@squaretrade.com>
2014-01-14 14:11:50 -0800

    string2 progress

--- basic/string2.py
+++ basic/string2.py
@@ -57,14 +57,14 @@ def front_back(a, b):
   # +++your code here+++
   def findMiddle(s):
     if len(s)%2 == 1:
-      indexHalf = len(s)/2 - 1.5
+      indexHalf = int(float(len(s))/2 - 0.5)
     else:
       indexHalf = len(s)/2
     return indexHalf
   middleA = findMiddle(a)
   middleB = findMiddle(b)
-  
-  return
+  finalString = a[:middleA] + b[:middleB] + a[middleA:] + b[middleB:]
+  return finalString
 
 
 # Simple provided test() function used in main() to print
